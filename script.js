7dece497b77882379d44a3effd108577865c5ec2
gauriiikaaa <gaurika2146.be21@chitkara.edu.in>
2023-10-24 18:22:55 +0530

    latest changes

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ $(".box") .click (function () {
     $ (".box") .removeClass ("active");
     $ (this) .addClass ("active");
 });
+const cartButtons = document.querySelectorAll('.add-to-cart');
+
+cartButtons.forEach(button => {
+
+  button.addEventListener('click', () => {
+    
+    alert('Item added to cart!');
+    
+  });
+
+});
